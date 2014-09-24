6aa268d759650111d4d5cf51cbc4efda8348930e
moljac <mcvjetko@holisticware.net>
2014-09-24 23:47:34 +0200

    Pages prepared

---
+++ tutorial-samples/Pages/Pages/PageMain.xaml.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Pages
+{	
+	public partial class PageMain : ContentPage
+	{	
+		public PageMain ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
