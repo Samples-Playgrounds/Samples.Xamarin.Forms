ce422f7748589de5c24c87c31610b3ac47be4251
moljac <mcvjetko@holisticware.net>
2014-12-07 23:51:41 +0100

    XAML samples for object model

---
+++ tools/HolisticWare.XamarinForms.Tools.XamlObjectModelScraper.EXE_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolisticWare.XamarinForms.Tools.XamlObjectModelScraper.EXE_Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
