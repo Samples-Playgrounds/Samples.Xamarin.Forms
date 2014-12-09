24737f25fdcd37731605047bdae1a17ff1cce7f6
moljac <mcvjetko@holisticware.net>
2014-12-09 23:09:46 +0100

    more xaml samples

--- tools/HolisticWare.XamarinForms.Tools.XamlObjectModelScraper.EXE_Console/Program.cs
+++ tools/HolisticWare.XamarinForms.Tools.XamlObjectModelScraper.EXE_Console/Program.cs
@@ -13,28 +13,71 @@ namespace HolisticWare.XamarinForms.Tools.XamlObjectModelScraper.EXE_Console
 	{
 		static void Main(string[] args)
 		{
-			XmlReader reader = XmlReader.Create("item1.xml");
-			XmlReader reader1 = XmlReader.Create("item2.xml");
-			XmlSchemaSet schemaSet = new XmlSchemaSet();
+			string[] files_xaml_to_infer =
+					System.IO.Directory.GetFiles
+					(
+					  ".",
+					  "*.xaml",
+					  System.IO.SearchOption.AllDirectories
+					 );
+
+
+			//http://quickstarts.asp.net/QuickStartv20/util/srcview.aspx?path=~/howto/samples/Xml/XmlSchemaInference/XmlSchemaInference.src&file=CS%5CXmlSchemaInference%5CXmlSchemaInference.cs&lang=C%23+Source
+
+			List<XmlSchemaSet> xml_schamesets = new List<XmlSchemaSet>();
+			XmlSchemaSet xml_schemaset_single = null;
+			XmlSchemaSet xml_schemaset_inferred = null;
 			XmlSchemaInference inference = new XmlSchemaInference();
-			schemaSet = inference.InferSchema(reader);
+			XmlWriter xml_writer = null;
+			XmlWriterSettings writter_settings = new XmlWriterSettings()
+			{
+				ConformanceLevel = ConformanceLevel.Auto
+			};
+
+			int i = 1;
+			foreach(string filename in files_xaml_to_infer)
+			{
+				Console.WriteLine("filename");
+				string filename_schema = @".\xamarin-forms" + i.ToString("d4") + ".xsd";
+
+				XmlReader reader = XmlReader.Create(filename);
+
+				// Use the additional data in xml to get the schema.
+				xml_schemaset_single =  new XmlSchemaSet();
+				xml_schemaset_single = inference.InferSchema(reader);
+				xml_schamesets.Add(xml_schemaset_single);
+
+				//write the new schema to file
+				xml_writer = XmlWriter.Create
+										(
+										new System.IO.StreamWriter(filename_schema),
+											writter_settings
+										);
+				System.IO.TextWriter text_writer = null;
+				j = 1;
+				foreach (XmlSchema schema in xml_schemaset_single.Schemas())
+				{
+					schema.Write(xml_writer);
+
+					j++;
+					Console.WriteLine("\t\t XSD = " + filename_schema);
+				}
+
+
+
+				xml_schemaset_inferred = inference.InferSchema(reader, xml_schemaset_inferred);
+
+
+				xml_writer.Flush();
+				xml_writer.Close();
+
+				i++;
+			}
+
 
 			// Display the inferred schema.
 			Console.WriteLine("Original schema:\n");
-			foreach (XmlSchema schema in schemaSet.Schemas("http://www.contoso.com/items"))
-			{
-				schema.Write(Console.Out);
-			}
 
-			// Use the additional data in item2.xml to refine the original schema.
-			schemaSet = inference.InferSchema(reader1, schemaSet);
-
-			// Display the refined schema.
-			Console.WriteLine("\n\nRefined schema:\n");
-			foreach (XmlSchema schema in schemaSet.Schemas("http://www.contoso.com/items"))
-			{
-				schema.Write(Console.Out);
-			}
 		}
 	}
 }
