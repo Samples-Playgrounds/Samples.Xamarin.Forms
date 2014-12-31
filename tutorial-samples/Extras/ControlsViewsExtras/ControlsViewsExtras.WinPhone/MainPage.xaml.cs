00e2517c3c14e5f1776d06bc1c22e23cbdbb28f0
moljac <mcvjetko@holisticware.net>
2014-12-31 21:08:37 +0100

    OxyPlot samples initial commit

---
+++ tutorial-samples/Extras/ControlsViewsExtras/ControlsViewsExtras.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Xamarin.Forms;
+
+
+namespace ControlsViewsExtras.WinPhone
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			OxyPlot.XamarinFormsWinPhone.Forms.Init();
+			Forms.Init();
+			Content = ControlsViewsExtras.App.GetMainPage().ConvertPageToUIElement(this);
+		}
+	}
+}
---
+++ tutorial-samples/Extras/ControlsViewsExtras/ControlsViewsExtras/App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ControlsViewsExtras
+{
+	public class App
+	{
+		public static Page GetMainPage()
+		{
+			return new ContentPage
+			{
+				Content = new OxyPlot.XamarinForms.PlotView
+				{
+					Model = new OxyPlot.PlotModel { Title = "Hello Xamarin.Forms" },
+					VerticalOptions = LayoutOptions.Fill,
+					HorizontalOptions = LayoutOptions.Fill,
+				},
+			};
+		}
+	}
+}
