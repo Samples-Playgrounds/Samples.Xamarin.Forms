133ea8f4dba2444af54aff18ccb3d03f4ca4550d
moljac <mcvjetko@holisticware.net>
2015-03-07 22:50:03 +0100

    project for samples - working with images

---
+++ tutorial-samples/Images/Images/Images/Views/ImagesEmbeddedResources.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Images.Views
+{
+	public partial class ImagesEmbeddedResources
+	{
+		public ImagesEmbeddedResources()
+		{
+			InitializeComponent();
+		}
+	}
+}
