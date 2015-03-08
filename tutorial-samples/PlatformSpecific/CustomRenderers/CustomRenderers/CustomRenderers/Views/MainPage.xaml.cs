734e88a87656c94a7f378c92eeda2ba3c3b461fa
moljac <mcvjetko@holisticware.net>
2015-03-08 22:23:54 +0100

    Custom BoxViewRenderer portable and Android version

---
+++ tutorial-samples/PlatformSpecific/CustomRenderers/CustomRenderers/CustomRenderers/Views/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CustomRenderers
+{
+	public partial class MainPage : ContentPage
+	{
+
+		protected Dictionary<string,string> samples = new Dictionary<string,string>()
+		{
+			{ "BoxView with Rounded Corners", "CustomRenderers.BoxViewCustomizations.PageBoxViewRoundedCorners"},
+			{ "ImageView Drawable", "PageBoxView"},
+		};
+
+		protected Dictionary<string,string> Samples
+		{
+			get;
+			set;
+		}
+
+		public MainPage ()
+		{
+			InitializeComponent ();
+
+			listViewSamples.ItemsSource = Samples = samples;
+
+			return;
+		}
+
+		public void OnItemSelected(object sender, ItemTappedEventArgs args_tapped)
+		{
+			object item = args_tapped.Item;
+
+			KeyValuePair<string, string> kvp = (KeyValuePair<string, string>) item;
+
+			string type_as_string = kvp.Value;
+
+			Type type = Type.GetType(type_as_string);
+			Page page = (Page) Activator.CreateInstance(type);
+
+			this.Navigation.PushAsync(page);
+
+			return;
+
+		}
+
+	}
+}
+
