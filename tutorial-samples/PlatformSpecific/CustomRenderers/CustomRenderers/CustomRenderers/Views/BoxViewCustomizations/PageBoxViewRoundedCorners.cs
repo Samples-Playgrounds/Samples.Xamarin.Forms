99fb57313237e5fdb29fb84b904b6ee168e88b6f
moljac <mcvjetko@holisticware.net>
2015-03-08 22:25:44 +0100

    BoxView custom Renderer iOS version + missing portable shared

---
+++ tutorial-samples/PlatformSpecific/CustomRenderers/CustomRenderers/CustomRenderers/Views/BoxViewCustomizations/PageBoxViewRoundedCorners.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CustomRenderers.BoxViewCustomizations
+{
+	public partial class PageBoxViewRoundedCorners : ContentPage
+	{
+
+		public PageBoxViewRoundedCorners ()
+		{
+			InitializeComponent ();
+
+			//this.boxViewRoundedCorners.CornerRadius = 90;
+
+			return;
+		}
+			
+	}
+}
+
