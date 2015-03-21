f08a3e2fcfd2012e1b486edb2c197c145cc0c2fd
moljac <mcvjetko@holisticware.net>
2015-03-21 23:54:24 +0100

    Map samples in TabbedPage

--- tutorial-samples/Basic-01-ControlsViews/Map/ControlsView.MapView/Views/Xaml/PageMap.xaml.cs
+++ tutorial-samples/Basic-01-ControlsViews/Map/ControlsView.MapView/Views/Xaml/PageMap.xaml.cs
@@ -6,19 +6,12 @@ using Xamarin.Forms.Maps;
 
 namespace ControlsView.MapView.Views.XAML
 {
-	public partial class PageMap : ContentPage
+	public partial class PageMap : TabbedPage
 	{
 		public PageMap ()
 		{
 			InitializeComponent ();
 
-			var position = new Position(37.79762, -122.40181);
-			Map.MoveToRegion(new MapSpan(position, 0.01, 0.01));
-			Map.Pins.Add(new Pin
-			{
-			    Label = "Xamarin",
-			    Position = position
-			});
 
 		}
 	}
