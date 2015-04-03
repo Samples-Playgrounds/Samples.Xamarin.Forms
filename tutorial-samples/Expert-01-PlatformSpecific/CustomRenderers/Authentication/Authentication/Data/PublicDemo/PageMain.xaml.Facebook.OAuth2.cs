823404d655876fbd27addd74d7c2aec46dc7a992
moljac <mcvjetko@holisticware.net>
2015-04-04 01:51:24 +0200

    cleanup

--- tutorial-samples/Expert-01-PlatformSpecific/CustomRenderers/Authentication/Authentication/Data/PublicDemo/PageMain.xaml.Facebook.OAuth2.cs
+++ tutorial-samples/Expert-01-PlatformSpecific/CustomRenderers/Authentication/Authentication/Data/PublicDemo/PageMain.xaml.Facebook.OAuth2.cs
@@ -8,14 +8,6 @@ namespace Authentication.Views
 		{
 			oauth2 = new HolisticWare.Auth.OAuth2()
 			{
-				/*
-				Vecernji List OK!!!
-
-				OAuth_IdApplication_IdAPI_KeyAPI_IdClient_IdCustomer = "117486881606473",
-				OAuth2_Scope = "", // "", "basic", "email",
-				OAuth_UriAuthorization = new Uri("https://m.facebook.com/dialog/oauth/"), 
-				OAuth_UriCallbackAKARedirect = new Uri("http://www.vecernji.hr"),
-				*/
 				OAuth_IdApplication_IdAPI_KeyAPI_IdClient_IdCustomer = "",
 				OAuth2_Scope = "", // "", "basic", "email",
 				OAuth_UriAuthorization = new Uri("https://m.facebook.com/dialog/oauth/"), 
