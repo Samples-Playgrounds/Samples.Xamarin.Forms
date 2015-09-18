f9d494ca3ca465c10095abe5a22fd91f5c616bcd
moljac Miljenko Cvjetko) <mcvjetko@holisticware.net>
2015-09-18 15:34:21 +0200

    xamarin forms sample portable project for testing

---
+++ diverse/project-created/20150918/AppPortable/AppPortable.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AppPortable.WinPhone.Resources;
+
+namespace AppPortable.WinPhone
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
