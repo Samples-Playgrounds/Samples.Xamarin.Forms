71fb148f2e8f370d718a38db1cc4705fbd44b2ae
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-04-29 14:22:31 -0400

    mapview

---
+++ tutorial-samples/Basic-01-ControlsViews/Map/MapView.Droid/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+
+// Required precission
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+
+namespace ControlsView.MapView.Droid
+{
+	[Activity(Label = "ControlsView.MapView", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public partial class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+			global::Xamarin.FormsMaps.Init(this, bundle);
+
+			LoadApplication(new App());
+		}
+	}
+}
+
---
+++ tutorial-samples/Basic-01-ControlsViews/Map/MapView/Views/XAML/PageMap.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace ControlsView.MapView.Views.XAML
+{
+	public partial class PageMap : TabbedPage
+	{
+		public PageMap ()
+		{
+			InitializeComponent ();
+
+
+		}
+	}
+}
+
