c1b8cafe7b3d7b13e593aba18d2f1734eca6f36b
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-04-29 14:42:09 -0400

    Pages sample

---
+++ tutorial-samples/Basic-04-Pages/Pages/PageNavigationPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pages
+{
+	public partial class PageNavigationPage
+	{
+		public PageNavigationPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
