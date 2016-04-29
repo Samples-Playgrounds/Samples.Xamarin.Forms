98567078eec4c5b2f310813f9876711df21942b8
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-04-29 14:40:11 -0400

    MasterDetailPage sample

---
+++ tutorial-samples/Basic-04-Pages/MasterDetailPageSample/MasterDetailPageSample/App.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MasterDetailPageSample
+{
+	public class App
+		: Application // superclass new in 1.3
+	{
+	    public App ()
+	    {
+	        // The root page of your application
+	        MainPage = 	// property
+			new MasterDetailPage
+			{
+				Master = 
+					new ContentPage()
+					{
+						Content =
+							new Label
+							{
+								Text = "Master",
+								VerticalOptions = LayoutOptions.CenterAndExpand,
+								HorizontalOptions = LayoutOptions.CenterAndExpand,
+							}
+					}
+				, Detail = 
+					new ContentPage()
+					{
+						Content  = 
+							new Label
+							{
+								Text = "Detail",
+								VerticalOptions = LayoutOptions.CenterAndExpand,
+								HorizontalOptions = LayoutOptions.CenterAndExpand,
+							}
+					}
+			};
+
+	        return;
+	    }
+
+		//public static Page GetMainPage()
+		//{
+		//	return 
+		//	new MasterDetailPage
+		//	{
+		//		Master = 
+		//			new ContentPage()
+		//			{
+		//				Content =
+		//					new Label
+		//					{
+		//						Text = "Master",
+		//						VerticalOptions = LayoutOptions.CenterAndExpand,
+		//						HorizontalOptions = LayoutOptions.CenterAndExpand,
+		//					}
+		//			}
+		//		, Detail = 
+		//			new ContentPage()
+		//			{
+		//				Content  = 
+		//					new Label
+		//					{
+		//						Text = "Detail",
+		//						VerticalOptions = LayoutOptions.CenterAndExpand,
+		//						HorizontalOptions = LayoutOptions.CenterAndExpand,
+		//					}
+		//			}
+		//	};
+		//}
+	}
+}
