5673ed8a505dc0f6e98d5f3d5df8458c99330ac8
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-04-29 14:41:07 -0400

    Navigation Page sample

---
+++ tutorial-samples/Basic-04-Pages/NavigationPageSample/NavigationPageSample/Views/PageContentUsedByNavigationPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NavigationPageSample
+{
+	public partial class PageContentUsedByNavigationPage : ContentPage
+	{
+		public PageContentUsedByNavigationPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
