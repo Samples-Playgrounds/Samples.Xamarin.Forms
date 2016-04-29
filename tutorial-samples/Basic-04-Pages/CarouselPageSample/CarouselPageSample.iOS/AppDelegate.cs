f808b7711c4570a6616868f754db489f5916ba7e
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-04-29 14:38:16 -0400

    CarouselPage

---
+++ tutorial-samples/Basic-04-Pages/CarouselPageSample/CarouselPageSample.iOS/AppDelegate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Xamarin.Forms;
+
+namespace CarouselPageSample.iOS
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the 
+	// User Interface of the application, as well as listening (and optionally responding) to 
+	// application events from iOS.
+	[Register("AppDelegate")]
+	public partial class AppDelegate 
+		: 
+		// UIApplicationDelegate
+		global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate // superclass new in 1.3
+	{
+		// class-level declarations
+		// UIWindow window;
+
+		//
+		// This method is invoked when the application has loaded and is ready to run. In this 
+		// method you should instantiate the window, load the UI into it and then make the window
+		// visible.
+		//
+		// You have 17 seconds to return from this method, or iOS will terminate your application.
+		//
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init ();
+
+	        LoadApplication (new App ());  // method is new in 1.3
+
+			return base.FinishedLaunching (app, options);
+
+			//window = new UIWindow(UIScreen.MainScreen.Bounds);
+			//window.RootViewController = App.GetMainPage().CreateViewController();
+			//window.MakeKeyAndVisible();
+
+			// return true;
+		}
+	}
+}
---
+++ tutorial-samples/Basic-04-Pages/CarouselPageSample/CarouselPageSample/App.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CarouselPageSample
+{
+	public class App
+		: Application // superclass new in 1.3
+	{
+	    public App ()
+	    {
+			CarouselPage cp =  new CarouselPage();
+
+			cp.Children.Add(new ContentPage() { Title = "ContentPage" });
+			cp.Children.Add(new ContentPage() { Title = "ContentPage" });
+			cp.Children.Add(new ContentPage() { Title = "ContentPage" });
+
+	        // The root page of your application
+	        MainPage = 	// property
+				cp
+				;
+				
+	        return;
+	    }
+
+		//public static Page GetMainPage()
+		//{
+		//	CarouselPage cp =  new CarouselPage();
+        //
+		//	cp.Children.Add(new ContentPage() { Title = "ContentPage" });
+		//	cp.Children.Add(new ContentPage() { Title = "ContentPage" });
+		//	cp.Children.Add(new ContentPage() { Title = "ContentPage" });
+		//	
+		//	return cp;
+		//}
+	}
+}
