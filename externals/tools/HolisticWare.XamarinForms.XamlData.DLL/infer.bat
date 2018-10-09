5572ef95947a6b23cc4f9e616a99b8b2370687d6
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-10-09 21:08:23 +0200

    externals tools

---
+++ externals/tools/HolisticWare.XamarinForms.XamlData.DLL/infer.bat
@@ -0,0 +1,45 @@
+"%ProgramFiles(x86)%"\XSDInference\bin\infer.exe ^
+	-v ^
+	"xaml-data\conceptdev-xamarin-forms-samples\BugSweeperPage.xaml" ^
+	"xaml-data\conceptdev-xamarin-forms-samples\CharacteristicDetail (2).xaml" ^
+	"xaml-data\conceptdev-xamarin-forms-samples\CharacteristicDetail.xaml" ^
+	"xaml-data\conceptdev-xamarin-forms-samples\CharacteristicDetail_Hrm.xaml" ^
+	"xaml-data\conceptdev-xamarin-forms-samples\CharacteristicDetail_TISensor.xaml" ^
+	"xaml-data\conceptdev-xamarin-forms-samples\CharacteristicDetail_TISwitch.xaml" ^
+	"xaml-data\conceptdev-xamarin-forms-samples\CharacteristicList (2).xaml" ^
+	"xaml-data\conceptdev-xamarin-forms-samples\CharacteristicList.xaml" ^
+	"xaml-data\conceptdev-xamarin-forms-samples\DeviceList (2).xaml" ^
+	"xaml-data\conceptdev-xamarin-forms-samples\DeviceList.xaml" ^
+	"xaml-data\conceptdev-xamarin-forms-samples\EmployeeListXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\MainPage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\MainPageXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\MyThirdNativePage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\NumericValidationTriggerXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\NumericValidationXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\OnClickEventTriggerXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\OneShotDateTimePage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\PersonPage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\RelativeLayoutPage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\RequiredFieldDataTriggerXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\SearchListXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\SearchPage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\SharedResourcesPage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\SimpleTriggerXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\SliderBindingsPage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\SliderTransformsPage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\SolitairePage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\StaticConstantsPage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\StyleXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\TabbedPageDemoPage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\TapInsideFrameXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\TipCalcPage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\TodoItemXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\UnevenRowsXaml.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\XamlBrowserPage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\XamlPlusCodePage.xaml" ^
+	"xaml-data\xamarin-xamarin-forms-samples\StyleXaml.xaml" ^ ^
+	-o inferred.xsd
+
+	
+	
+pause
