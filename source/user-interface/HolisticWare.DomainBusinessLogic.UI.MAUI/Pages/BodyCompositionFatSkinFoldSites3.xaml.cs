fe014cb4a5b3cb0cb482a35b286f1b3ec3e13195
moljac <mcvjetko@holisticware.net>
2021-08-20 10:59:22 +0300

    source files

---
+++ source/user-interface/HolisticWare.DomainBusinessLogic.UI.MAUI/Pages/BodyCompositionFatSkinFoldSites3.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace HolisticWare.DomainBusinessLogic.UI.MAUI
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BodyCompositionFatSkinFoldSites3 : ContentPage, IPage
+	{
+		public BodyCompositionFatSkinFoldSites3()
+		{
+			InitializeComponent();
+		}
+
+		int count = 0;
+
+		private void OnButtonClicked(object sender, EventArgs e)
+		{
+		}
+	}
+}
